c707e69f5a979852b74cda80c87acad80d73739d
JayPatel4739 <pjay51477@gmail.com>
2025-05-25 08:53:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
-;var errormessage = "";
-var missingval = "";
-
 function isEmail(email) {
-    var regex = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email);
+  var regex = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
 }
 
 function isStrongPassword(password) {
-    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,15}$/;
-    return regex.test(password);
+  var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,15}$/;
+  return regex.test(password);
 }
 
 function isValidPhone(phone) {
-    return /^\d{10}$/.test(phone);
+  return /^\d{10}$/.test(phone);
 }
 
-$("#submitbut").click(function () {
-    // Reset messages
-    errormessage = "";
-    missingval = "";
+$(document).ready(function () {
+  $(".toggle-password").click(function () {
+    const inputId = $(this).data("target");
+    const input = $("#" + inputId);
+    const type = input.attr("type") === "password" ? "text" : "password";
+    input.attr("type", type);
+    $(this).toggleClass("fa-eye fa-eye-slash");
+  });
 
-    // Email validation
-    var email = $("#Email").val();
+  $("#submitbut").click(function () {
+    let errormessage = "";
+    let missingval = "";
+
+    const email = $("#Email").val().trim();
+    const phone = $("#PhoneNo").val().trim();
+    const password = $("#password").val();
+    const confirmPass = $("#confirmpass").val();
+
     if (email === "") {
-        missingval += "<p>Email is Not Added</p>";
+      missingval += "<p>Email is not added.</p>";
     } else if (!isEmail(email)) {
-        errormessage += "<p>Email is not valid</p>";
+      errormessage += "<p>Email is not valid.</p>";
     }
 
-    // Password validation
-    var password = $("#password").val();
-    var confirmPass = $("#confirmpass").val();
-    if (password === "") {
-        missingval += "<p>Password is Not Added</p>";
-    } else if (!isStrongPassword(password)) {
-        errormessage += "<p>Password must be 8-15 characters, include 1 uppercase, 1 lowercase, and 1 special character</p>";
-    } else if (password !== confirmPass) {
-        errormessage += "<p>Passwords Don't Match</p>";
-    }
-
-    // Phone number validation
-    var phone = $("#PhoneNo").val();
     if (phone === "") {
-        missingval += "<p>Phone Number is not added</p>";
+      missingval += "<p>Phone number is not added.</p>";
     } else if (!isValidPhone(phone)) {
-        errormessage += "<p>Phone Number must be exactly 10 digits</p>";
+      errormessage += "<p>Phone number must be exactly 10 digits.</p>";
     }
 
-    // Output result
-    if (missingval === "" && errormessage === "") {
-        $("#success").html("You're Registered!");
-        $("#errors").html("");
-    } else {
-        $("#errors").html(errormessage + missingval);
-        $("#success").html("");
+    if (password === "") {
+      missingval += "<p>Password is not added.</p>";
+    } else if (!isStrongPassword(password)) {
+      errormessage += "<p>Password must be 8-15 characters, include 1 uppercase, 1 lowercase, and 1 special character.</p>";
+    } else if (password !== confirmPass) {
+      errormessage += "<p>Passwords do not match.</p>";
     }
+
+    if (missingval === "" && errormessage === "") {
+      $("#success").html("You're Registered!").fadeIn();
+      $("#errors").html("").hide();
+    } else {
+      $("#errors").html(errormessage + missingval).fadeIn();
+      $("#success").html("").hide();
+    }
+  });
 });
